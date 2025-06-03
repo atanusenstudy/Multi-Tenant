d2a45223b7e27fd2d81185215dc7960bbbffc4b7
atanusenstudy <atanusenstudy@gmail.com>
2025-06-03 09:05:20 +0530

    Added Required packages to infrastructure

---
+++ WebAPI/ReadMe.cs
@@ -0,0 +1,41 @@
+﻿
+public class ReadMe
+{
+    //This class serves as a placeholder for documentation or metadata about the WebAPI project.
+    #region Common
+    /*
+     * Clean Layered Architecture: This Project references to infrastructure which references to Application and which references to Domain.
+     * 
+     */
+    #endregion
+    #region webapi
+    /*
+     * 
+     */
+    #endregion
+
+    #region infrastructure
+    /*
+     * Housing the External Dependencies , Database Context, Repositories, and Migrations.
+     * What Db are we using etc
+     */
+    #endregion
+
+    #region domain
+    /* 
+     * Housing the Domain Entities(Representation of Database Tables), Domain Services, Domain Events, and Domain Value Objects.
+     */
+    #endregion
+
+    #region application
+    /*
+     *  House CQRS Implementations, Application Services, DTOs, Mappings, and MediatR Handlers. 
+     * 
+     */
+    #endregion
+    #region common
+    /*
+     * 
+     */
+    #endregion
+}
